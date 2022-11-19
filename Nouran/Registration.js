bdf7b1b9ed85ec524e035d0907194f927aa14cab
Batool <batool.com>
2022-11-19 22:25:24 +0300

    update

--- Nouran/Registration.js
+++ Nouran/Registration.js
@@ -32,3 +32,26 @@ function signup() {
     }
 
 }
+
+
+
+// function ValidateEmail(p) {
+//     var mailformat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+//     var email = p.value;
+//     console.log(email);
+//     if (!(email.match(mailformat))) {
+//         alert("This is not a valid email address");
+//         return false;
+//     }
+// }
+
+
+// function ValidatePassword(p) {
+//     var passFormat = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+//     var pass = p.value;
+//     console.log(pass);
+//     if (!(pass.match(passFormat))) {
+//         alert("This is not a valid Password");
+//         return false;
+//     }
+// }
